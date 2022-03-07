ab5fa1c259b3fbd1167b77812277e2cb198258d5
manh2001827 <66905530+manh2001827@users.noreply.github.com>
2022-03-07 14:39:15 +0700

    tet

--- api/routes/cartRoute.js
+++ api/routes/cartRoute.js
@@ -1,7 +1,8 @@
 const controller = require("../controller/cart")
+var cors = require('cors')
 
 const Router = (app)=>{
-    app.get('/cart/:id',controller.getCart)
+    app.get('/cart/:id',cors(),controller.getCart)
     app.post('/cart',controller.addCart)
     app.put('/cart/:id',controller.updateCart)
     app.delete('/cart/:id',controller.deleteCart)
