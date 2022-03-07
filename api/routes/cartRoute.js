ede3d7078b8c11f24d53baf35bddf1a5a65f5ec6
manh2001827 <66905530+manh2001827@users.noreply.github.com>
2022-03-07 12:27:03 +0700

    adsa

--- api/routes/cartRoute.js
+++ api/routes/cartRoute.js
@@ -1,7 +1,7 @@
 const controller = require("../controller/cart")
 
 const Router = (app)=>{
-    app.get('/cart',controller.getCart)
+    app.get('/cart/:id',controller.getCart)
     app.post('/cart',controller.addCart)
     app.put('/cart/:id',controller.updateCart)
     app.delete('/cart/:id',controller.deleteCart)
